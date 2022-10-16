667d2b5a25d351fdd578615b362cabe0d5be3186
Fernando Vegh <vegh.fernando@gmail.com>
2022-10-16 13:42:58 -0300

    Atualização

--- main_script.R
+++ main_script.R
@@ -1,1 +1,12 @@
 
+
+# Carregando os dados:
+dados <- read_excel('FEV-data-Excel.xlsx')
+
+# Visualizando o dataset:
+View(dados)
+
+# Valores NA no dataset?
+sum(is.na(dados)) # Há valores NAs presente
+
+str(dados) # Algumas variáveis precisam ser modificadas para tipo fator
