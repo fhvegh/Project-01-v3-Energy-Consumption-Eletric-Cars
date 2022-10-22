7ea8a09a2549a9b855bb72634d51ab3bb9e5b328
Fernando Vegh <vegh.fernando@gmail.com>
2022-10-22 12:18:03 -0300

    Atualização Arquivo

---
+++ Analise_Exploratoria.R
@@ -0,0 +1,9 @@
+
+# Análise Exploratória
+
+setwd("C:/My_Development/Projetos/Energy-Consumption-Eletric-Cars/Energy-Consumption-Eletric-Cars")
+
+# Carregando os dados:
+dados.num <- read.csv("dados.num.csv")
+dados.cat <- read.csv("dados.cat.csv")
+
